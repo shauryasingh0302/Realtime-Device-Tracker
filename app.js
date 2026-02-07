fc31d7ba4a08a175f67b965709df626bc3c04303
Shaurya Singh <shauryasingh0302@icloud.com>
2026-02-07 19:59:08 +0530

    final commit

--- app.js
+++ app.js
@@ -25,4 +25,7 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
